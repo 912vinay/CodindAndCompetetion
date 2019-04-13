c5d7855d458c27c8837fb372e90f46e48a71374d
912vinay <912vinay@gmail.com>
2019-04-13 12:19:05 +0530

    Coding Competition Code

--- src/com/vinay/lombog/Emp.java
+++ src/com/vinay/lombog/Emp.java
@@ -2,11 +2,7 @@
 package com.vinay.lombog;
 
 
-import jdk.nashorn.internal.objects.annotations.Getter;
-import jdk.nashorn.internal.objects.annotations.Setter;
 
-@Getter
-@Setter
 
 public class Emp
 {
